0621b6f75a1f04c5745b257d260ea498a68fe34e
ankitpandey006 <ankitpandey03052005@gmail.com>
2024-08-05 01:16:10 +0530

    add js

---
+++ JAVA Script/app.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+let a = 5;
+let b = 2;
+console.log("sum is = ",a+b);
+
+let pencil_price = 10;
+let ereser_prince = 5;
+// let output = "The total price is : "+(pencil_price + ereser_prince)+"Rupees"
+let output = `The tota prince is : ${pencil_price+ereser_prince}rupess`;
+console.log(output);
